efe26c3cf419c80900cd76d002a376a47eb3537d
KiIlerDrift <75575748+KiIlerDrift@users.noreply.github.com>
2022-04-28 22:52:32 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ from pumpkin import pumkins
 from raddish import raddishes
 from onion import onions
 
-option = ""
+options = ""
 
 def menu():
     print("")
@@ -13,16 +13,19 @@ def menu():
     print(f"Please select a option from the list below:")
     print("")
     print(
-    f"\n1. Add Password"
-    f"\n2. View Passwords"
-    f"\n3. Generate Passwords"
-    f"\n4. Search Password"
+    f"\n1. Melons"
+    f"\n2. Pumpkins"
+    f"\n3. Raddish"
+    f"\n4. Onions"
     f"\n5. End"
     "\n ")
     globals() ['option'] = int(input("\nEnter your option 1-5 here: "))
+
+menu()
 
 while option != 0:
     if option == 1: melons() ; menu()
     elif option == 2: pumkins() ; menu()
     elif option == 3: raddishes() ; menu()
     elif option == 4: onions() ; menu()
+    elif option == 5: exit()
